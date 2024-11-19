c1772697c2a64b7410ea80cbc5e35655f4ead727
Brian Wanjohi <brian@aegirconsult.systems>
2024-11-19 17:35:48 +0300

    Add files via upload

---
+++ bootstrap/ssr/assets/TextInput-SoXY5mdM.js
@@ -0,0 +1,93 @@
+import { mergeProps, useSSRContext, useModel, ref, onMounted } from "vue";
+import { ssrRenderAttrs, ssrInterpolate, ssrRenderSlot, ssrGetDynamicModelProps } from "vue/server-renderer";
+const _sfc_main$2 = {
+  __name: "InputError",
+  __ssrInlineRender: true,
+  props: {
+    message: {
+      type: String
+    }
+  },
+  setup(__props) {
+    return (_ctx, _push, _parent, _attrs) => {
+      _push(`<div${ssrRenderAttrs(mergeProps({
+        style: __props.message ? null : { display: "none" }
+      }, _attrs))}><p class="text-sm text-red-600">${ssrInterpolate(__props.message)}</p></div>`);
+    };
+  }
+};
+const _sfc_setup$2 = _sfc_main$2.setup;
+_sfc_main$2.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("resources/js/Components/InputError.vue");
+  return _sfc_setup$2 ? _sfc_setup$2(props, ctx) : void 0;
+};
+const _sfc_main$1 = {
+  __name: "InputLabel",
+  __ssrInlineRender: true,
+  props: {
+    value: {
+      type: String
+    }
+  },
+  setup(__props) {
+    return (_ctx, _push, _parent, _attrs) => {
+      _push(`<label${ssrRenderAttrs(mergeProps({ class: "block text-sm font-medium text-gray-700" }, _attrs))}>`);
+      if (__props.value) {
+        _push(`<span>${ssrInterpolate(__props.value)}</span>`);
+      } else {
+        _push(`<span>`);
+        ssrRenderSlot(_ctx.$slots, "default", {}, null, _push, _parent);
+        _push(`</span>`);
+      }
+      _push(`</label>`);
+    };
+  }
+};
+const _sfc_setup$1 = _sfc_main$1.setup;
+_sfc_main$1.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("resources/js/Components/InputLabel.vue");
+  return _sfc_setup$1 ? _sfc_setup$1(props, ctx) : void 0;
+};
+const _sfc_main = {
+  __name: "TextInput",
+  __ssrInlineRender: true,
+  props: {
+    "modelValue": {
+      type: String,
+      required: true
+    },
+    "modelModifiers": {}
+  },
+  emits: ["update:modelValue"],
+  setup(__props, { expose: __expose }) {
+    const model = useModel(__props, "modelValue");
+    const input = ref(null);
+    onMounted(() => {
+      if (input.value.hasAttribute("autofocus")) {
+        input.value.focus();
+      }
+    });
+    __expose({ focus: () => input.value.focus() });
+    return (_ctx, _push, _parent, _attrs) => {
+      let _temp0;
+      _push(`<input${ssrRenderAttrs((_temp0 = mergeProps({
+        class: "rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500",
+        ref_key: "input",
+        ref: input
+      }, _attrs), mergeProps(_temp0, ssrGetDynamicModelProps(_temp0, model.value))))}>`);
+    };
+  }
+};
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("resources/js/Components/TextInput.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+export {
+  _sfc_main$1 as _,
+  _sfc_main as a,
+  _sfc_main$2 as b
+};
